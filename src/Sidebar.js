d520811d0dae882e64a864a391d870eaf7cadcb2
= <=>
2020-10-10 21:04:21 +0200

    101020:2104

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,19 +1,55 @@
 import { Avatar,IconButton } from '@material-ui/core';
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './sidebar.css';
 import SearchIcon from '@material-ui/icons/Search';
 import RateReviewOutlinedIcon from '@material-ui/icons/RateReviewOutlined';
 import SidebarChat from './SidebarChat';
 import {useSelector} from 'react-redux';
 import {selectUser} from './features/userSlice';
-import { auth } from './firebase';
+import db,{ auth } from './firebase';
+
 
 function Sidebar() {
 
     
+     const [chats,setChats]  = useState([]);
 
     const user = useSelector(selectUser);
 
+    useEffect(() => {
+        
+        db.collection("chats").onSnapshot((snapshot) => 
+
+            
+           setChats(
+               
+            snapshot.docs.map((doc) => ({
+
+                id: doc.id,
+                data: doc.data(),
+           }))
+         )
+        
+
+     );
+
+     
+
+    },[]);
+
+    const addChat = () => {
+
+        const chatName = prompt("Enter a chat name");
+
+        if(chatName){
+
+            db.collection("chats").add({
+                chatName: chatName,
+            });
+
+        }
+       
+    };
 
     return (
         <div className="sidebar">
@@ -31,16 +67,24 @@ function Sidebar() {
              </div>
 
             <IconButton variant ="outlined" className="inputButton">
-             <RateReviewOutlinedIcon />
+             <RateReviewOutlinedIcon onClick={addChat}/>
              </IconButton>
 
             </div>
 
             <div className="sidebar_chats">
 
-            <SidebarChat />
-            <SidebarChat />
-            <SidebarChat />
+                {
+                    chats.map(({id, data:{chatName}}) =>(
+
+                        <SidebarChat key={id} id={id}  chatName={chatName}/>
+
+                    ))
+                 }
+                       
+
+
+                    
 
           
 
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import { Avatar } from '@material-ui/core'
 import React from 'react';
 import './SidebarChat.css';
 
-function SidebarChat() {
+function SidebarChat({id,chatName}) {
     return (
         <div className="sidebarchat">
 
@@ -10,7 +10,7 @@ function SidebarChat() {
             
             <div className="sidebarchat_info">
 
-                <h3>Channel Name</h3>
+                <h3>{chatName}</h3>
                 <p>Last message...</p>
                 <small>timestamp</small>
 
