815e0493625b0ce4be4ec271d5aa0d8125b1b141
= <=>
2020-10-10 12:21:18 +0200

    101020:1221

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,32 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <h2>Sidebar</h2>
+
+            <div className="sidebar_header">
+
+            <Avatar />
+            
+            <div className="input">
+
+
+                < SearchIcon />
+
+                <input type="text" placeholder="Search" />
+            
+             </div>
+
+            </div>
+
+            <div className="sidebar_chat">
+
+
+            </div>
             
         </div>
     )
