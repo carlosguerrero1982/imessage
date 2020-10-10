cd7ef24d1b5aaab1d0c81367a267da08ea5e2d89
= <=>
2020-10-10 12:06:55 +0200

    101020:1206

---
+++ src/Imessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Imessage.css';
+import Sidebar from './Sidebar';
+
+function Imessage() {
+    return (
+        <div className="imessage">
+
+            < Sidebar />
+
+
+        </div>
+    )
+}
+
+export default Imessage
