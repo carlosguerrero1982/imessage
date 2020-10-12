032242009496e25dcb17c32b6c9bd6cf122f7d70
= <=>
2020-10-12 16:56:56 +0200

    end

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Message.css';
 import {Avatar} from '@material-ui/core';
-
+import {selectUser} from './features/userSlice';
+import {useSelector} from 'react-redux';
 
 function Message({id,contents:{timestamp,displayName,email,message,photo,uid}}) {
+  
+    const user = useSelector(selectUser);
+  
     return (
-        <div className="message">
+        <div className={`message ${user.email===email && "message_sender" }`}>
             < Avatar src={photo} />
                 <p>{message}</p>
             <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,14 +1,30 @@
 import { Avatar } from '@material-ui/core'
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { setChat } from './features/chatSlice';
 import './SidebarChat.css';
 import {useDispatch} from 'react-redux';
+import db from './firebase';
 
 function SidebarChat({id,chatName}) {
 
     const dispatch = useDispatch();
 
     const [chatInfo, setChatInfo] = useState([]);
+
+    useEffect(() => {
+       
+        db.collection("chats").doc(id).collection("messages").
+        orderBy("timestamp","desc").onSnapshot(snapshot=>{
+
+            setChatInfo(snapshot.docs.map(doc => doc.data()
+            
+        
+            ))
+        })
+
+
+        
+    }, [id])
 
 
     return (
@@ -26,14 +42,14 @@ function SidebarChat({id,chatName}) {
         
        } } className="sidebarchat">
 
-            < Avatar />
+            < Avatar src={chatInfo[0]?.photo} />
             
             <div className="sidebarchat_info">
 
                 <h3>{chatName}</h3>
             
-                <p>Last message...</p>
-                <small>timestamp</small>
+                <p>{chatInfo[0]?.message}</p>
+                 <small>{new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()}</small>
 
            </div>     
             
